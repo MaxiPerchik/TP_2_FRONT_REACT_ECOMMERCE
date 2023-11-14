8fa2db3788671c34a894689231377eff62643c0b
Maxi <mperchik@protonmail.com>
2023-11-13 22:22:11 -0300

    manejo de errores y modifiacion de codigo

--- src/Componentes/User/UserRegister.jsx
+++ src/Componentes/User/UserRegister.jsx
@@ -2,135 +2,176 @@ import React, { useState } from "react";
 import Swal from "sweetalert2";
 
 const UserRegister = () => {
-  const [nombre, setNombre] = useState("");
-  const [apellido, setApellido] = useState("");
-  const [telefono, setTelefono] = useState("");
-  const [email, setEmail] = useState("");
-  const [emailConfirmacion, setEmailConfirmacion] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    nombre: "",
+    apellido: "",
+    telefono: "",
+    email: "",
+    emailConfirmacion: "",
+    password: "",
+  });
+
   const [error, setError] = useState("");
 
-  const manejadorFormulario = (event) => {
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const manejadorFormulario = async (event) => {
     event.preventDefault();
 
-    if (
-      !nombre ||
-      !apellido ||
-      !telefono ||
-      !email ||
-      !emailConfirmacion ||
-      !password
-    ) {
+    const { nombre, apellido, email, emailConfirmacion, password } = formData;
+
+    if (!(nombre && apellido && email && emailConfirmacion && password)) {
       setError("Por favor completar todos los campos");
       return;
     }
 
-    // Validamos que los campos del email coincidan
     if (email !== emailConfirmacion) {
-      setError("El mail ingresado no coincide");
+      setError("El correo electrónico ingresado no coincide");
       return;
     }
 
     const user = {
       username: nombre + apellido,
-      email: email,
-      password: password,
+      email,
+      password,
     };
 
-    console.log({ ...user });
-
     try {
-      // Realizamos la solicitud de registro al servidor
-      console.log("entras o no?");
-      const response = fetch("http://localhost:3000/api/users/register", {
+      const response = await fetch("http://localhost:3000/api/users/register", {
         method: "POST",
         mode: "cors",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ ...user }),
+        body: JSON.stringify(user),
       });
 
-      if (response) {
-        // Registro exitoso, limpiamos los campos y mostramos la alerta
+      if (response.ok) {
         limpiarCampos();
         Swal.fire({
           title: "<strong>Registro Exitoso!!!</strong>",
-          html:
-            "<i>El usuario <strong>" +
-            nombre +
-            " " +
-            apellido +
-            " </strong>  fue agregado correctamente en el listado</i>",
+          html: `<i>El usuario <strong>${nombre} ${apellido}</strong> fue agregado correctamente en el listado</i>`,
           icon: "success",
         });
       } else {
-        // Manejar el caso de error en la respuesta del servidor
-        setError("Error al registrar el usuario");
+        const { success, error } = await response.json();
+        setError(success ? "Error al registrar el usuario" : error);
       }
     } catch (error) {
-      // Manejar errores de red u otros errores
+      setError("Error al realizar la solicitud: " + error.message);
       console.error("Error al realizar la solicitud:", error);
     }
   };
 
   const limpiarCampos = () => {
-    setNombre("");
-    setApellido("");
-    setTelefono("");
-    setEmail("");
-    setEmailConfirmacion("");
+    setFormData({
+      nombre: "",
+      apellido: "",
+      telefono: "",
+      email: "",
+      emailConfirmacion: "",
+      password: "",
+    });
     setError("");
-    setPassword("");
   };
 
   return (
-    <div>
-      <h2>Registro</h2>
+    <div className="container">
+      <div className="row">
+        <div className="col-4 col-6">
+          <h2>Registro</h2>
 
-      <form onSubmit={manejadorFormulario}>
-        <div>
-          <label htmlFor="">Nombre</label>
-          <input type="text" onChange={(e) => setNombre(e.target.value)} />
+          <form onSubmit={manejadorFormulario}>
+            <div>
+              <label htmlFor="nombre" className="form-label">
+                Nombre
+              </label>
+              <input
+                type="text"
+                name="nombre"
+                value={formData.nombre}
+                onChange={handleChange}
+                className="form-control"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="apellido" className="form-label">
+                Apellido
+              </label>
+              <input
+                type="text"
+                name="apellido"
+                value={formData.apellido}
+                onChange={handleChange}
+                className="form-control"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="telefono" className="form-label">
+                Telefono
+              </label>
+              <input
+                type="text"
+                name="telefono"
+                value={formData.telefono}
+                onChange={handleChange}
+                className="form-control"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="form-control"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="emailConfirmacion" className="form-label">
+                Email Confirmacion
+              </label>
+              <input
+                type="email"
+                name="emailConfirmacion"
+                value={formData.emailConfirmacion}
+                onChange={handleChange}
+                className="form-control"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="form-control"
+              />
+            </div>
+
+            <button className="btn btn-success mt-3" type="submit">
+              Registrar
+            </button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+          </form>
         </div>
-
-        <div>
-          <label htmlFor="">Apellido</label>
-          <input type="text" onChange={(e) => setApellido(e.target.value)} />
-        </div>
-
-        <div>
-          <label htmlFor="">Telefono</label>
-          <input type="text" onChange={(e) => setTelefono(e.target.value)} />
-        </div>
-
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="email" onChange={(e) => setEmail(e.target.value)} />
-        </div>
-
-        <div>
-          <label htmlFor="">Email Confirmacion</label>
-          <input
-            type="email"
-            onChange={(e) => setEmailConfirmacion(e.target.value)}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="">Password</label>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        {error && <p style={{ color: "red" }}>{error}</p>}
-
-        <button className="btn btn-success" type="submit">
-          Registrar
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
