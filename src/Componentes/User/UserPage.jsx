13e34ce536752fb10c541c54462e693ec3788a55
Maxi <mperchik@protonmail.com>
2023-11-17 11:15:14 -0300

    + react-toaster +- codigo con toaster

--- src/Componentes/User/UserPage.jsx
+++ src/Componentes/User/UserPage.jsx
@@ -1,45 +1,58 @@
 import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const UserPage = () => {
   const [users, setUsers] = useState([]);
-  const [error, setError] = useState(null);
+
+  const notifyError = (errorMessage) => {
+    toast.error(errorMessage, {
+      position: "top-right",
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      // style: {
+      //   background: "#d9534f", // Background color for error
+      //   color: "#fff", // Text color for error
+      // },
+      className: "toast-error",
+    });
+  };
+
+  const fetchData = async () => {
+    try {
+      // recuperamos el token
+      const token = localStorage.getItem("token");
+
+      // chequeamos si el token existe
+      const headers = token
+        ? { "Content-Type": "application/json", Authorization: `Bearer ${token}` }
+        : { "Content-Type": "application/json" };
+
+      // solicito a la API
+      const response = await fetch("http://localhost:3000/api/users", { headers });
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error || `${data.status}, ${data.error}`);
+      }
+
+      setUsers(data);
+    } catch (error) {
+      notifyError(error.message);
+    }
+  };
 
   useEffect(() => {
-    // Obtener el token almacenado en localStorage
-    const token = localStorage.getItem("token");
-
-    // Configurar el encabezado de autorización con el token si está presente
-    const headers = token
-      ? {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        }
-      : {
-          "Content-Type": "application/json",
-        };
-
-    // Realizar la solicitud con el encabezado de autorización
-    fetch("http://localhost:3000/api/users", { headers })
-      .then(async (response) => {
-        if (!response.ok) {
-          // Capturar el mensaje de error del servidor
-          const data = await response.json();   
-          throw new Error(data.error || "Error desconocido del servidor");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setUsers(data);
-      })
-      .catch((error) => setError(error.message));
+    fetchData();
   }, []);
-
-  if (error) {
-    return <p className="container">Error: {error}</p>;
-  }
 
   return (
     <div className="container">
+      <ToastContainer />
       <div className="row">
         <ul className="col-8">
           {users.map((user) => (
