4a23f5f43db68ca980ece9e50c21181446259db2
ibriangithub <ibriangithub@gmail.com>
2024-02-21 22:56:39 -0300

    pusheo

--- src/Componentes/Product/ProductDetail.jsx
+++ src/Componentes/Product/ProductDetail.jsx
@@ -9,7 +9,9 @@ const ProductDetail = () => {
   useEffect(() => {
     const loadProduct = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/api/products/searchNombre/${name}`);
+        const response = await fetch(
+          `http://localhost:3000/api/products/searchNombre/${name}`
+        );
         if (!response.ok) {
           throw new Error();
         }
@@ -40,18 +42,26 @@ const ProductDetail = () => {
       <div className="row justify-content-center">
         <div className="col-6 ">
           <div className="card">
-            <img src={product.productImage} className="card-img-top" alt="..."  style={{ maxHeight: "400px", objectFit: "fill" }}/>
+            <img
+              src={product.productImage}
+              className="card-img-top"
+              alt="..."
+              style={{ maxHeight: "400px", objectFit: "fill" }}
+            />
             <div className="card-body">
               <h5 className="card-title">{product.productName}</h5>
               <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-              <div class="card-footer text-end"><h6>{product.productPrice}</h6></div>
+              <div class="card-footer text-end">
+                <h6>{product.productPrice}</h6>
+              </div>
             </div>
           </div>
         </div>
